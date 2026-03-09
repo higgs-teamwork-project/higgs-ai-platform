82d7b03bae18dd886f5f6f76f7bdfbbc75056da0
leolin25 <leolinuk25@gmail.com>
2026-03-09 10:56:41 +0000

    doc: added documentation for frontend team to sync

--- backend/main_api.py
+++ backend/main_api.py
@@ -67,7 +67,7 @@ async def register_account(data: RegisterData):
         return {"status": "success", "message": f"Account created with ID {account_id}"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
+        
 @app.get("/api/donors")
 async def get_all_donors():
     doners = database.dataset_db.list_donors()
