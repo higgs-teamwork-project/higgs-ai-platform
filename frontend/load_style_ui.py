d75dd9e6b7067d3979aa033350c301d5918f5316
afrahhh27 <afraibnathossain@gmail.com>
2026-03-16 15:11:00 +0000

    update: added some comments to make using style sheet easier

--- frontend/load_style_ui.py
+++ frontend/load_style_ui.py
@@ -1,6 +1,22 @@
 import os
 from pathlib import Path
 
+"""
+Using the style sheet
+style sheet file: stylesheet.qss
+
+Works like css. Use Qt reference to add styles & for syntax.
+The property 'styling' of a widget will add a style from the stylesheet to it.
+Adding styles:
+QWidget[styling="mystyle"]{
+....
+}
+On the corresponding widget, say myWidget = QWidget()
+Set the property: myWidget.setProperty("styling", "mystyle")
+If style doesn't load it is due to style inheritance hierarchy. Layout widgets cannot be styled.
+"""
+
+
 def loadstylesheet():
     try: 
         PATH = Path(__file__).parent
