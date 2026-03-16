a39c0ab1b8f7774db2d1129e58f76eec934657ef
Fan-nnn <feiie00755@gmail.com>
2026-03-16 03:52:06 +0000

    feat: add setting page

--- frontend/dashboard_ui.py
+++ frontend/dashboard_ui.py
@@ -6,7 +6,7 @@ from PySide6.QtCore import Qt
 
 # ---- THE MAGIC LINK: Import your sub-pages here! ----
 from prompt_ui import HIGGSApp
-#from admin_settings_ui import AdminSettingsWindow
+from setting_ui import SettingsWindow
 
 class DashboardWindow(QMainWindow):
     def __init__(self, parent_window=None):
@@ -34,7 +34,7 @@ class DashboardWindow(QMainWindow):
 
         self.nav_settings_btn = QPushButton("Settings")
         self.nav_settings_btn.setStyleSheet("padding: 5px 15px; margin: 5px; color: #C12250; border: 1px solid #C12250; border-radius: 3px; background-color: transparent;")
-        self.nav_settings_btn.clicked.connect(self.open_admin_dashboard)
+        self.nav_settings_btn.clicked.connect(self.open_settings_page)
         nav_layout.addWidget(self.nav_settings_btn)
 
         main_layout.addLayout(nav_layout)
@@ -87,7 +87,7 @@ class DashboardWindow(QMainWindow):
             self.prompt_window = HIGGSApp()
             self.prompt_window.dashboard_window = self
             
-        # self.admin_window = AdminSettingsWindow(parent_window=self)
+        self.settings_window = SettingsWindow(parent_window=self)
 
 
     def open_prompt_page(self):
@@ -96,6 +96,10 @@ class DashboardWindow(QMainWindow):
 
     def open_admin_dashboard(self):
         QMessageBox.information(self, "Coming Soon", "The Admin module is currently under development.")
+
+    def open_settings_page(self):
+        self.hide() 
+        self.settings_window.show()
 
     def open_profile_page(self):
         QMessageBox.information(self, "Coming Soon", "The Schedule module is currently under development.")
