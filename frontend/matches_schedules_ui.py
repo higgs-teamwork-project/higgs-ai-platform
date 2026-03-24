3999643abd8ccec9670a258d35c2fa6a403fa2a6
afrahhh27 <afraibnathossain@gmail.com>
2026-03-24 18:34:11 +0000

    feat: made progress bar wait whilst AI loads

--- frontend/matches_schedules_ui.py
+++ frontend/matches_schedules_ui.py
@@ -1,4 +1,5 @@
-from PySide6.QtCore import (Qt)
+from PySide6.QtCore import (Qt,
+                            QThreadPool)
 from PySide6.QtWidgets import (QWidget,
                                QSplitter,
                                QMainWindow,
@@ -7,7 +8,8 @@ from PySide6.QtWidgets import (QWidget,
                                QHBoxLayout,
                                QPushButton,
                                QTabWidget,
-                               QMessageBox)
+                               QMessageBox,
+                               QProgressBar)
 
 import requests
 from load_style_ui import loadstylesheet
@@ -17,6 +19,7 @@ from schedule_ui import generate_schedule, Schedule
 from navbar_ui import HNavBar
 from datetime import datetime, date, time 
 import sys
+from run_api_req import RequestWorker
 
 """
 !! This is the main output window.
@@ -121,7 +124,7 @@ class MatchesTabView(QWidget):
     def __init__(self, donor_id):
         super().__init__()
         self.donor_id = donor_id
-
+        self.threadpool = QThreadPool()
         # --- layout ---
         self.tab_layout = QVBoxLayout(self)
 
@@ -137,6 +140,11 @@ class MatchesTabView(QWidget):
         self.matches_outer_layout = QVBoxLayout(self.matches_tab)    
         self.matches_tab.setLayout(self.matches_outer_layout)
 
+        self.matches_spinner = QProgressBar()
+        self.matches_spinner.setRange(0, 0)
+        self.matches_spinner.setTextVisible(False)
+        self.matches_outer_layout.addWidget(self.matches_spinner)
+
         data = self.parse_matches_table()
         self.matches_table = MatchesTable(data) # data = [] if no saved matches
         self.matches_outer_layout.addWidget(self.matches_table)
@@ -146,6 +154,7 @@ class MatchesTabView(QWidget):
         self.generate_matches_btn.clicked.connect(self.generate_match)
         self.matches_outer_layout.addWidget(self.generate_matches_btn, alignment=Qt.AlignmentFlag.AlignCenter)
 
+        self.matches_spinner.hide()
         if len(data) == 0:
             self.matches_table.hide()
         else:
@@ -162,6 +171,9 @@ class MatchesTabView(QWidget):
         # --- set up ---
         self.tab_layout.addWidget(self.tab_view)
         self.setLayout(self.tab_layout)
+
+    def return_results(self, d):
+        return d
 
     def parse_matches_table(self):
         try:
@@ -207,17 +219,32 @@ class MatchesTabView(QWidget):
             QMessageBox.critical(self, "Server Error", "Could not retrieve schedule. Please try again later.")
             return None
 
-    def generate_match(self):
+    def get_recs_req(self, id):
         try:
-            response = requests.get(f"http://127.0.0.1:8000/api/donors/{self.donor_id}/recommendations?top_k=22 &save_matches=True")
+            response = requests.get(f"http://127.0.0.1:8000/api/donors/{id}/recommendations?top_k=22 &save_matches=True")
             data = response.json()
             print(data)
-            self.load_matches_table()
-            self.make_donor_schedule(data["recommendations"])
+            return data
         except Exception as e:
             QMessageBox.critical(self, "Server Error", "Could not generate matches. Please try again later.")
             print("ERROR: "+ e)
             return None
+        
+    def handle_new_matches(self, data):
+        self.matches_spinner.hide()
+        self.load_matches_table()
+        self.make_donor_schedule(data["recommendations"])
+
+
+    def generate_match(self):
+        self.generate_matches_btn.hide()
+        self.matches_spinner.show()
+        worker = RequestWorker(
+            self.get_recs_req,
+            self.donor_id
+        )
+        worker.signals.result.connect(self.handle_new_matches)
+        self.threadpool.start(worker)
     
     def load_matches_table(self):
         self.generate_matches_btn.hide()
